b4afc604d455f6ef14c90ae0300190abe1783308
yangyongjian <76436092@qq.com>
2019-03-11 15:09:56 +0800

    chat action picker

--- src/stories/Chat.stories.js
+++ src/stories/Chat.stories.js
@@ -6,7 +6,24 @@ import '../components/Icons/index.scss';
 import chatHtml from '../components/Chat/index.html';
 
 storiesOf('Chat 聊天', module)
-  .add('default', () => chatHtml, {
+  .add('default', () => {
+    let container = document.createElement('div')
+    container.innerHTML = chatHtml
+
+    let btn = container.querySelector('#chatactions')
+    let picker = container.querySelector('#chatactionsPicker')
+    function toggleActions () {
+      if (picker.classList.contains('show')) {
+        picker.classList.add('hide')
+        picker.classList.remove('show')
+      } else if (picker.classList.contains('hide')) {
+        picker.classList.add('show')
+        picker.classList.remove('hide')
+      }
+    }
+    btn.addEventListener('click', toggleActions)
+    return container
+  }, {
     codes: chatHtml,
   })
 ;
